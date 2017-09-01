e5ab1202316b610e64eaaae3d740993314d6d0fe
Yattsu <yattsu@users.noreply.github.com>
2017-09-01 16:25:55 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 prefs = {
-	'token': 'MzE1NjAxMDkyNzQwMTIwNTc4.DDrI-Q.ws0jg21Xaf_3MuWpHRtr7-VxiV8',
-	'webhook': 'https://discordapp.com/api/webhooks/352496662385786886/ZHvkle0WfCEwVajBuzK8s5HgzpN7KGoBc608pPV0dpKJBQtABGZRfgroKzvHCUPKe4Fn',
-	'server_id': '325256820400586753',
+	'token': '',
+	'webhook': '',
+	'server_id': '',
 	'username': 'Message',
 	'avatar_url': 'https://shecantrade.com/media/SheCanTrade/site-images/icon-two.png?ext=.png'
 }
